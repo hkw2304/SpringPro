2566f1c28bd29ef41b60d3fe5a6f32fae74fb9e8
potato <User@DESKTOP-TFS6VDA>
2024-01-03 09:36:40 +0900

    메인페이지 초안/ 국어,영어,수학 url 경로 설정

---
+++ spring_boot01_Project/src/main/java/com/example/demo/controller/mainPage.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class mainPage {
+	@RequestMapping(value="/searchPage")
+	public ModelAndView searchPage() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("searchPage");
+		return mav;
+	}
+	@RequestMapping(value="/korBook")
+	public ModelAndView korBook() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("korBook");
+		return mav;
+	}
+	@RequestMapping(value="/engBook")
+	public ModelAndView engBook() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("engBook");
+		return mav;
+	}
+	@RequestMapping(value="/mathBook")
+	public ModelAndView mathBook() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mathBook");
+		return mav;
+	}
+}
